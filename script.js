2fda5b026ade2a9ef7aaa3db7edb6c7b804ccdfe
Gerard Darris <darrisgr@gmail.com>
2019-10-10 20:24:35 -0500

    Added script to verify all forms are filled with valid data

--- script.js
+++ script.js
@@ -11,3 +11,22 @@
 </ol>
 <img src="${}">
 */
+
+window.addEventListener("load", function(){
+   let form = document.querySelector("form");
+   form.addEventListener("submit", function(event){
+      let pilotNameInput = document.querySelector("input[name=pilotName]");
+      let copilotNameInput = document.querySelector("input[name=copilotName]");
+      let fuelLevel = document.querySelector("input[name=fuelLevel]");
+      let cargoWeight = document.querySelector("input[name=cargoWeight]");
+      let pilotTag = document.getElementById("pilotName");
+      let copilotTag = document.getElementById("copilotName");
+      if (pilotNameInput.value === "" || copilotNameInput.value === "" || fuelLevel.value === "" || cargoWeight.value === "") {
+         alert("All fields are required!");
+         event.preventDefault();
+      } else if (!isNaN(pilotNameInput.value) || !isNaN(copilotNameInput.value) || isNaN(fuelLevel.value) || isNaN(cargoWeight.value)) {
+         alert("Make sure to enter valid information for each field!");
+         event.preventDefault();
+      }
+   });
+});
