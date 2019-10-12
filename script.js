958e75f8333475c0dcab6145ef1be91da3614bf2
Gerard Darris <darrisgr@gmail.com>
2019-10-11 22:10:29 -0500

    added JS code to complete alerts and shuttle requirements instructions

--- script.js
+++ script.js
@@ -12,21 +12,50 @@
 <img src="${}">
 */
 
-window.addEventListener("load", function(){
-   let form = document.querySelector("form");
-   form.addEventListener("submit", function(event){
+window.addEventListener("load", function () {
+   let button = document.getElementById("formSubmit");
+   button.addEventListener("click", function (event) {
       let pilotNameInput = document.querySelector("input[name=pilotName]");
       let copilotNameInput = document.querySelector("input[name=copilotName]");
       let fuelLevel = document.querySelector("input[name=fuelLevel]");
       let cargoWeight = document.querySelector("input[name=cargoWeight]");
-      let pilotTag = document.getElementById("pilotName");
-      let copilotTag = document.getElementById("copilotName");
+      let launchStatus = document.getElementById("launchStatus");
+      let faultyItems = document.getElementById("faultyItems");
+      let pilotStatus = document.getElementById("pilotStatus");
+      let copilotStatus = document.getElementById("copilotStatus");
+      let fuelStatus = document.getElementById("fuelStatus");
+      let cargoStatus = document.getElementById("cargoStatus");
+      
       if (pilotNameInput.value === "" || copilotNameInput.value === "" || fuelLevel.value === "" || cargoWeight.value === "") {
          alert("All fields are required!");
          event.preventDefault();
-      } else if (!isNaN(pilotNameInput.value) || !isNaN(copilotNameInput.value) || isNaN(fuelLevel.value) || isNaN(cargoWeight.value)) {
+      }
+      else if (!isNaN(pilotNameInput.value) || !isNaN(copilotNameInput.value) || isNaN(fuelLevel.value) || isNaN(cargoWeight.value)) {
          alert("Make sure to enter valid information for each field!");
          event.preventDefault();
       }
+      else if (fuelLevel.value <= 10000 || cargoWeight.value >= 10000) {
+         launchStatus.innerHTML = "Shuttle Not Ready for Launch";
+         launchStatus.style.color = "red";
+         faultyItems.style.visibility = "visible";
+         pilotStatus.innerHTML = `Pilot ${pilotNameInput.value} is ready for launch.`;
+         copilotStatus.innerHTML = `Co-pilot ${copilotNameInput.value} is ready for launch.`;
+         fuelStatus.innerHTML = "Fuel level high enough for launch";
+         cargoStatus.innerHTML = "Cargo weight low enough for launch";
+         
+         if (fuelLevel.value <= 10000) {
+            fuelStatus.innerHTML = "Fuel level too low for launch";
+         }
+         else if (cargoWeight.value >= 10000) {
+            cargoStatus.innerHTML = "Cargo weight too high for launch";
+         };
+         event.preventDefault();
+      }
+      else {
+         faultyItems.style.visibility = "hidden";
+         launchStatus.style.color = "green";
+         launchStatus.innerHTML = "Shuttle Is Ready for Launch";
+         event.preventDefault();
+      };
    });
 });
