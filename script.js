9e2ab78ee9235c3171d8420ee3e7b5dddfca52dc
Gerard Darris <darrisgr@gmail.com>
2019-10-14 18:22:05 -0500

    Updated script.js

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
-// Write your JavaScript code here!
-
-/* This block of code shows how to format the HTML once you fetch some planetary JSON!
-<h2>Mission Destination</h2>
-<ol>
-   <li>Name: ${}</li>
-   <li>Diameter: ${}</li>
-   <li>Star: ${}</li>
-   <li>Distance from Earth: ${}</li>
-   <li>Number of Moons: ${}</li>
-</ol>
-<img src="${}">
-*/
+window.addEventListener("load", function () {
+   let missionTarget = document.getElementById("missionTarget");
+   let indexVal = Math.floor(Math.random() * 6);
+   fetch("https://handlers.education.launchcode.org/static/planets.json").then(function (response) {
+      response.json().then(function (json) {
+         missionTarget.innerHTML = `
+               <h2>Mission Destination</h2>
+               <ol>
+                  <li>Name: ${json[indexVal].name}</li>
+                  <li>Diameter: ${json[indexVal].diameter}</li>
+                  <li>Star: ${json[indexVal].star}</li>
+                  <li>Distance from Earth: ${json[indexVal].distance}</li>
+                  <li>Number of Moons: ${json[indexVal].moons}</li>
+               </ol>
+               <img src="${json[indexVal].image}">
+               `;
+      });
+   });
+});
 
 window.addEventListener("load", function () {
    let button = document.getElementById("formSubmit");
@@ -73,12 +79,12 @@ window.addEventListener("load", function () {
       };
    });
 
-   button.addEventListener("click", function() {
+   button.addEventListener("click", function () {
       let missionTarget = document.getElementById("missionTarget");
       let indexVal = Math.floor(Math.random() * 6);
       if (fetchReady && !notEnoughInputs && !invalidInputs) {
-         fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
-            response.json().then(function(json) {
+         fetch("https://handlers.education.launchcode.org/static/planets.json").then(function (response) {
+            response.json().then(function (json) {
                missionTarget.innerHTML = `
                   <h2>Mission Destination</h2>
                   <ol>
@@ -92,8 +98,6 @@ window.addEventListener("load", function () {
                   `;
             });
          });
-      } else {
-         missionTarget.innerHTML = ``;
       };
    });
 });
